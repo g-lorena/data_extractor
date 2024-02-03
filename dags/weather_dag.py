4d95c8a8b42f08197d7947f2d48c7a1c9cfd187d
lgongang <lorena.gongang.partner@decathlon.com>
2024-02-04 00:04:37 +0100

    added information in the DAGs

---
+++ dags/weather_dag.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from openAPI_extract import extract_data
+
+# Define your Airflow DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
+
+dag = DAG('run_data_extractor', default_args=default_args, schedule="40 5 16 * *")
+
+run_extract_apidata = PythonOperator(
+    task_id='extract_data_from_API',
+    python_callable=extract_data,
+    dag=dag
+)
+run_extract_apidata
